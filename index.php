cb4ca840e759863dc6d745a6afb3701a25ed2484
jdc20181 <jdc20181@gmail.com>
2016-07-01 18:27:10 -0400

    Create index.php
    Test File

---
+++ index.php
@@ -0,0 +1,8 @@
+<FORM METHOD=POST ACTION=post.php>
+Your Name: <INPUT TYPE=text NAME=name maxLength="20"><br>
+Message: <TEXTAREA cols=20 rows=10 NAME=msg></TEXTAREA>
+<BR><BR><INPUT TYPE=submit VALUE='Post the messege'></FORM>
+<br><Br><br>
+<?php
+include "msgs";
+?>
